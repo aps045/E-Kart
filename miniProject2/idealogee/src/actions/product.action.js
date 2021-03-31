b19eca21f1cc3e5fff2e326e76362779c019c70f
ayush singhal <ayush.singhal_cs18@gla.ac.in>
2021-03-31 17:58:24 +0530

    delte

--- miniProject2/idealogee/src/actions/product.action.js
+++
@@ -1,9 +0,0 @@
-import axios from "../helpers/axios";
-
-export const addProduct= form=>{
-    console.log("axios");
-    return async dispatch=>{
-        const res=await axios.post(`/product/create`,form);
-        console.log(res);
-    }
-}
