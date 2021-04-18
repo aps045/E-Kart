307daffb2629b2d06b44661e5f9ce1ce18605e23
ayush singhal <ayush.singhal_cs18@gla.ac.in>
2021-04-18 19:48:18 +0530

    project all files

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router} from 'react-router-dom'
+import {DataProvider} from './GlobalState'
+import Header from './components/headers/Header'
+import MainPages from './components/mainpages/Pages'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DataProvider>
+      <Router>
+        <div className="App">
+          <Header />
+          <MainPages />
+        </div>
+      </Router>
+    </DataProvider>
   );
 }
 
